dde69fdab194f19e47a7cfb7ddc8de6aa1d2a7cb
kylevillepo2 <kvillepo@gmail.com>
2024-12-21 17:34:01 -0800

    changes

--- YOLO_inference.py
+++ YOLO_inference.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO("models/best.pt")
+model = YOLO("yolo11n.pt")
 
 result = model.predict("data/TennisMatch1.mp4", conf=0.2,save=True)
 print(result)
-result2 = model.predict("data/streetPic.png.webp")
-print(result2)
+# result2 = model.predict("data/streetPic.png.webp")
+# print(result2)
 
 print("boxes:")
 for box in result[0].boxes:
